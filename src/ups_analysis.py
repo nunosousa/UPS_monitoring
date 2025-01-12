57ea9d46bdf16b202297ff8d96607338d5734cfd
Nuno Sousa <sousa.nms@gmail.com>
2025-01-12 15:10:20 +0000

    fixed time

--- src/ups_analysis.py
+++ src/ups_analysis.py
@@ -1,7 +1,9 @@
 import polars
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 #dataset format:
+# %TIME @Y@m@d @H@M@S% %VAR battery.charge% %VAR input.voltage% %VAR ups.load% [%VAR ups.status%] %VAR ups.temperature% %VAR input.frequency%
 # 20250109 180822 NA 222.0 2 [OL BYPASS] 29.0 50.0
 
 ups_data_frame = polars.read_csv(
@@ -23,6 +25,9 @@ ups_data_frame = ups_data_frame.with_columns(
 ups_data_frame = ups_data_frame.with_columns(
     polars.col("column_4").cast(polars.Float32))
 
+ups_data_frame = ups_data_frame.with_columns(
+    polars.col("column_1").dt.combine(polars.col("column_2")).alias("DateTime"))
+
 ups_data_frame = ups_data_frame.rename({"column_1": "Date", "column_2": "Time","column_4": "Voltage"})
 
 dates = ups_data_frame.unique(subset="Date", maintain_order=True)
@@ -31,13 +36,14 @@ num_dates = dates.height
 
 for i in range(num_dates):
     ups_data_frame_day = ups_data_frame.filter(polars.col("Date") == dates.item(i, 0))
+    #print(ups_data_frame_day)
 
     # Norma CEI 38 2003+
     # 230 V -10% +6% (entre 207V e 243,8V)
     cei38_high = 243.8
     cei38_low = 207.0
 
-    x_cei38 = [0, ups_data_frame_day.height-1]
+    x_cei38 = [ups_data_frame_day["DateTime"].item(0), ups_data_frame_day["DateTime"].item(ups_data_frame_day.height-1)]
     y_high_cei38 = [cei38_high, cei38_high]
     y_low_cei38 = [cei38_low, cei38_low]
 
@@ -46,7 +52,9 @@ for i in range(num_dates):
 
     ax1 = fig.add_subplot(2, 1, 1)
 
-    ax1.plot(ups_data_frame_day["Voltage"], label='Voltage')
+    #ax1.plot(ups_data_frame_day["Voltage"], label='Voltage')
+    ax1.plot(ups_data_frame_day["DateTime"], ups_data_frame_day["Voltage"], label='Voltage')
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
     ax1.set_ylim(top=cei38_high+4, bottom=cei38_low-4)
     ax1.set_xlim(left=x_cei38[0], right=x_cei38[1])
     ax1.set_xlabel('Time')
@@ -59,12 +67,8 @@ for i in range(num_dates):
 
 
     ax2 = fig.add_subplot(2, 1, 2)
-    
+
     ax2.set_xlabel('Time')
-
-
-
-    print(ups_data_frame_day["Time"].item(0))
 
 plt.show()
 
